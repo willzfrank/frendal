876505bc64704584cc24d40883fe94d6df7405f9
willzfrank <willzudoh4@gmail.com>
2022-08-30 10:08:07 +0100

    added nav css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,10 +7,10 @@ const Navbar = () => {
   return (
     <NavbarContainer>
       <img src={logo} alt="" />
-      <div>
+      <ProfileContainer>
         <p>Register</p>
         <img src={profile} alt="" />
-      </div>
+      </ProfileContainer>
     </NavbarContainer>
   );
 };
@@ -18,6 +18,37 @@ const Navbar = () => {
 export default Navbar;
 
 const NavbarContainer = styled.nav`
-  border: 2px solid red;
+  /* border: 2px solid red; */
   display: flex;
+  justify-content: space-between;
+
+  img {
+    height: 132px;
+    width: 305px;
+    border-radius: 0px;
+  }
+`;
+
+const ProfileContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 73px;
+  justify-content: center;
+
+  p {
+    font-size: 24px;
+    font-weight: 600;
+    line-height: 33px;
+    letter-spacing: 0em;
+    text-align: left;
+    cursor: pointer;
+  }
+
+  img {
+    height: 75px;
+    width: 75px;
+    left: 1311px;
+    top: 21px;
+    border-radius: 50%;
+  }
 `;
